39b2dbab6ce310208d33e127bccbbd74e151d65b
ParikshitKaushik <122871202+ParikshitKaushik@users.noreply.github.com>
2023-11-01 09:55:30 +0530

    Mail.ino

--- Mail.ino
+++ Mail.ino
@@ -2,40 +2,40 @@
 #include <ArduinoJson.h>
 #include <ArduinoHttpClient.h>
 
-const char* ssid = "Ankush";
-const char* pass = "12345678";
+const char* ssid = "Ankush"; // Your WiFi network name
+const char* pass = "12345678"; // Your WiFi password
 
-const char* IFTTT_WEBHOOK_EVENT = "doorLock";
-const char* IFTTT_KEY = "djmTxByajgaXmwohDns7rvPK_Nsk553vDqSaO1saWzK";
+const char* IFTTT_WEBHOOK_EVENT = "doorLock"; // IFTTT event name
+const char* IFTTT_KEY = "djmTxByajgaXmwohDns7rvPK_Nsk553vDqSaO1saWzK"; // IFTTT Maker Webhooks key
 
-const char* server = "maker.ifttt.com";
-const int port = 80;
+const char* server = "maker.ifttt.com"; // IFTTT server address
+const int port = 80; // HTTP port
 
 const int buttonPin = 2;  // Define the pin connected to your push button
 
 int lastButtonState = LOW;
 int buttonState = LOW;
 
-WiFiClient wifi;
-HttpClient client = HttpClient(wifi, server, port);
+WiFiClient wifi; // Create a WiFiClient instance
+HttpClient client = HttpClient(wifi, server, port); // Create an HttpClient instance for IFTTT
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200); // Initialize serial communication
   delay(10);
-  
-  pinMode(buttonPin, INPUT_PULLUP);  // Use the internal pull-up resistor
 
-  connectWiFi();
+  pinMode(buttonPin, INPUT_PULLUP);  // Configure the pin for the push button and use the internal pull-up resistor
+
+  connectWiFi(); // Connect to WiFi
 }
 
 void loop() {
-  buttonState = digitalRead(buttonPin);
+  buttonState = digitalRead(buttonPin); // Read the state of the push button
 
   if (buttonState == LOW && lastButtonState == HIGH) {
-    sendEmail("Someone at the door!");
+    sendEmail("Someone at the door!"); // Send an email when the button is pressed
   }
 
-  lastButtonState = buttonState;
+  lastButtonState = buttonState; // Store the current button state for the next iteration
 }
 
 void connectWiFi() {
@@ -43,7 +43,7 @@ void connectWiFi() {
     Serial.println("Connected to WiFi");
   } else {
     Serial.println("WiFi connection failed");
-    while (1);
+    while (1); // End the program if WiFi connection fails
   }
 }
 
@@ -52,14 +52,10 @@ void sendEmail(const char* comment) {
   String url = "/trigger/" + String(IFTTT_WEBHOOK_EVENT) + "/with/key/" + String(IFTTT_KEY);
   url += "&value1=" + String(comment);
 
-  client.get(url);
+  client.get(url); // Send an HTTP GET request to IFTTT
 
-  int statusCode = client.responseStatusCode();
-  //Serial.print("Status code: ");
+  int statusCode = client.responseStatusCode(); // Get the HTTP response status code
   Serial.println(statusCode);
 
- // if (statusCode == -3) {
-    Serial.println("Email sent successfully!");
- //
-  delay(1000);  // Debounce the button
+  delay(1000);  // Debounce the button to prevent multiple presses
 }
